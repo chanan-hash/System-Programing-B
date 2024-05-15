1ab091343e17d9b38b165c5f310b66e205aa2525
chanan-hash <chananhelman@gmail.com>
2024-05-15 15:33:36 +0300

    Adding tests

--- Assignment2/Test.cpp
+++ Assignment2/Test.cpp
@@ -26,6 +26,106 @@ TEST_CASE("Test graph addition")
     ariel::Graph g4;
     g4.loadGraph(expectedGraph);
     CHECK((g3 == g4) == true);
+    CHECK((&g3 != &g1) == true);
+
+    SUBCASE("Test graph addition with assignment")
+    {
+        g1 += g2;
+        CHECK((g1 == g4) == true);
+        CHECK((g1 != g3) == false);
+    }
+
+    SUBCASE("Test unary +")
+    {
+        ariel::Graph g5 = +g1;
+        CHECK((g5 == g1) == true);
+    }
+
+    SUBCASE("Addition of two graphs with different dimensions")
+    {
+        ariel::Graph g6;
+        vector<vector<int>> graph2 = {
+            {0, 1, 0, 0, 1},
+            {1, 0, 1, 0, 0},
+            {0, 1, 0, 1, 0},
+            {0, 0, 1, 0, 1},
+            {1, 0, 0, 1, 0}};
+        g6.loadGraph(graph2);
+        CHECK_THROWS(g1 + g6);
+        CHECK_THROWS(g1 += g6);
+    }
+
+    SUBCASE("Test algorithms")
+    {
+        CHECK(g3.getDirected() == false);
+        CHECK(ariel::Algorithms::isConnected(g3) == true);
+        CHECK(ariel::Algorithms::isContainsCycle(g3) == true);
+        CHECK(ariel::Algorithms::isBipartite(g3) == "The graph is not bipartite");
+        CHECK(ariel::Algorithms::shortestPath(g3, 1, 2) == "1 -> 2");
+    }
+}
+
+TEST_CASE("Test graph subtraction")
+{
+    ariel::Graph g1;
+    vector<vector<int>> graph = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 1, 0}};
+    g1.loadGraph(graph);
+    ariel::Graph g2;
+    vector<vector<int>> weightedGraph = {
+        {0, 1, 1},
+        {1, 0, 2},
+        {1, 2, 0}};
+    g2.loadGraph(weightedGraph);
+    ariel::Graph g3 = g1 - g2;
+
+    vector<vector<int>> expectedGraph = {
+        {0, 0, -1},
+        {0, 0, -1},
+        {-1, -1, 0}};
+    ariel::Graph g4;
+    g4.loadGraph(expectedGraph);
+    CHECK((g3 == g4) == true);
+
+    SUBCASE("Test graph subtraction with assignment")
+    {
+        g1 -= g2;
+        CHECK((g1 == g4) == true);
+        CHECK((g1 != g3) == false);
+    }
+
+    SUBCASE("Test unary -")
+    {
+        g1 -= g2;
+        ariel::Graph g5 = -g1;
+        g4 = -g4;
+        CHECK((g5 == g4) == true);
+
+        g5 = -g5;
+        vector<vector<int>> unsignedGraph = {
+            {0, 0, 1},
+            {0, 0, 1},
+            {1, 1, 0}};
+        ariel::Graph g6;
+        g6.loadGraph(unsignedGraph);
+        CHECK((g5 == g1) == true);
+    }
+
+    SUBCASE("Subtraction of two graphs with different dimensions")
+    {
+        ariel::Graph g6;
+        vector<vector<int>> graph2 = {
+            {0, 1, 0, 0, 1},
+            {1, 0, 1, 0, 0},
+            {0, 1, 0, 1, 0},
+            {0, 0, 1, 0, 1},
+            {1, 0, 0, 1, 0}};
+        g6.loadGraph(graph2);
+        CHECK_THROWS(g1 - g6);
+        CHECK_THROWS(g1 -= g6);
+    }
 }
 
 TEST_CASE("Test graph multiplication")
