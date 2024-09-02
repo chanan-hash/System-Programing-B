c86133b501894db94e7f5824f2d27e644ea59727
chanan-hash <chananhelman@gmail.com>
2024-09-02 14:12:19 +0300

    Adding more files

---
+++ Assignment4/complex.hpp
@@ -0,0 +1,44 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <sstream>
+
+using namespace std;
+
+// This is the class for the complex numbers
+
+class Complex
+{
+private: // the definition of the complex number
+    double real;
+    double imaginary;
+
+public:
+    Complex(double real, double imaginary);
+
+    // Getters
+    double get_real() const;
+    double get_imaginary() const;
+
+    /**
+     * Checks if two complex numbers are equal.
+     * Two complex numbers are equal if their real and imaginary parts are equal.
+     * @param other The other complex number to compare with.
+     */
+    bool operator==(const Complex &other) const;
+
+    // String representation of the complex number
+    string to_string() const;
+
+    // comparison operators
+    bool operator<(const Complex &other);
+    bool operator>(const Complex &other);
+    bool operator<=(const Complex &other);
+    bool operator>=(const Complex &other);
+
+    /**
+     * Overloads the << operator to print the complex number.
+     */
+    friend ostream &operator<<(ostream &os, const Complex &complex);
+};
