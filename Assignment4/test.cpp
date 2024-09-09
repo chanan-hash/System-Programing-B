68d612063bdfd9388d7a2270f1ffbc38cf198f56
chanan-hash <chananhelman@gmail.com>
2024-09-09 18:19:45 +0300

    Adding more test

--- Assignment4/test.cpp
+++ Assignment4/test.cpp
@@ -16,7 +16,7 @@ TEST_CASE("Testing the complex class")
     CHECK_FALSE(c1 == c2);
     CHECK(c1 != c2);
     CHECK_FALSE(c1 < c2);
-    CHECK_FALSE(c1 > c2); 
+    CHECK_FALSE(c1 > c2);
     CHECK(c1 <= c2);
     CHECK(c1 >= c2);
     CHECK(c1.to_string() == "1+i2");
@@ -155,4 +155,98 @@ TEST_CASE("empty tree")
         DFS += to_string((it)->get_value()) + " ";
     }
     CHECK(DFS == "");
+}
+
+// testing the 4-tree with complex numbers
+TEST_CASE("Checking 4-tree with complex numbers")
+{
+    Node<Complex> node_cmpx1(Complex(1, 2));
+    Node<Complex> node_cmpx2(Complex(1, -2));
+    Node<Complex> node_cmpx3(Complex(3, 4));
+    Node<Complex> node_cmpx4(Complex(5, 6));
+    Node<Complex> node_cmpx5(Complex(7, 8));
+    Node<Complex> node_cmpx6(Complex(9, 10));
+    Node<Complex> node_cmpx7(Complex(11, -12));
+    Node<Complex> node_cmpx8(Complex(13, 14));
+    Node<Complex> node_cmpx9(Complex(15, 16));
+
+    Tree<Complex, 4> tree_cmpx;
+    tree_cmpx.add_root(&node_cmpx1);
+    tree_cmpx.add_sub_node(&node_cmpx1, &node_cmpx2);
+    tree_cmpx.add_sub_node(&node_cmpx1, &node_cmpx3);
+    tree_cmpx.add_sub_node(&node_cmpx1, &node_cmpx4);
+    tree_cmpx.add_sub_node(&node_cmpx1, &node_cmpx5);
+    tree_cmpx.add_sub_node(&node_cmpx2, &node_cmpx6);
+    tree_cmpx.add_sub_node(&node_cmpx2, &node_cmpx7);
+    tree_cmpx.add_sub_node(&node_cmpx2, &node_cmpx8);
+    tree_cmpx.add_sub_node(&node_cmpx2, &node_cmpx9);
+
+    // checking the BFS
+    SUBCASE("Checking BFS")
+    {
+        string BFS = "";
+        for (auto it = tree_cmpx.begin_BFS(); it != tree_cmpx.end_BFS(); ++it)
+        {
+            BFS += (it)->get_value().to_string() + " ";
+        }
+        CHECK(BFS == "1+i2 1-i2 3+i4 5+i6 7+i8 9+i10 11-i12 13+i14 15+i16 ");
+    }
+
+    // checking the DFS
+    SUBCASE("Checking DFS")
+    {
+        string DFS = "";
+        for (auto it = tree_cmpx.begin_DFS(); it != tree_cmpx.end_DFS(); ++it)
+        {
+            DFS += (it)->get_value().to_string() + " ";
+        }
+        CHECK(DFS == "1+i2 1-i2 9+i10 11-i12 13+i14 15+i16 3+i4 5+i6 7+i8 ");
+
+        // checking the Pre order
+        string pre_order = "";
+        for (auto it = tree_cmpx.begin_pre_order(); it != tree_cmpx.end_pre_order(); ++it)
+        {
+            pre_order += (it)->get_value().to_string() + " ";
+        }
+        CHECK(pre_order == "1+i2 1-i2 9+i10 11-i12 13+i14 15+i16 3+i4 5+i6 7+i8 ");
+        CHECK(pre_order == DFS);
+    }
+    // checking the In order
+    SUBCASE("Checking In order")
+    {
+        string in_order = "";
+        for (auto it = tree_cmpx.begin_in_order(); it != tree_cmpx.end_in_order(); ++it)
+        {
+            in_order += (it)->get_value().to_string() + " ";
+        }
+        CHECK(in_order == "1+i2 1-i2 9+i10 11-i12 13+i14 15+i16 3+i4 5+i6 7+i8 ");
+    }
+
+    // checking the Post order
+    SUBCASE("Checking Post order")
+    {
+        string post_order = "";
+        for (auto it = tree_cmpx.begin_post_order(); it != tree_cmpx.end_post_order(); ++it)
+        {
+            post_order += (it)->get_value().to_string() + " ";
+        }
+        CHECK(post_order == "1+i2 1-i2 9+i10 11-i12 13+i14 15+i16 3+i4 5+i6 7+i8 ");
+    }
+
+    // checking the Tree to Heap
+    SUBCASE("Checking Tree to Heap")
+    {
+        string tree_to_heap = "";
+        for (auto it = tree_cmpx.begin_TreeToHeap(); it != tree_cmpx.end_TreeToHeap(); ++it)
+        {
+            tree_to_heap += (it)->get_value().to_string() + " ";
+        }
+        CHECK(tree_to_heap == "11-i12 1+i2 1-i2 3+i4 5+i6 7+i8 9+i10 13+i14 15+i16 ");
+    }
+
+    SUBCASE("Adding a child to a full parent")
+    {
+        Node<Complex> node_cmpx10(Complex(17, 18));
+        CHECK_THROWS(tree_cmpx.add_sub_node(&node_cmpx1, &node_cmpx10));
+    }
 }
